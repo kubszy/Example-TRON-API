5ab8b9b91405cea7584df67707b6145d33106f9e
kubszy <jakub@szynal.com.pl>
2020-06-21 20:19:27 +0000

    Create API/TronController and methods: createWallet, getWalletBalance

---
+++ app/Http/Controllers/API/TronController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Wallet;
+
+class TronController extends Controller
+{
+
+    public function createWallet()
+    {
+
+    }
+
+
+    public function getWalletBalance($address)
+    {
+
+    }
+}
