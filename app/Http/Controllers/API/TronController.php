25a9dacd09902217ebfb4e1c95dbcd2a771e629c
kubszy <jakub@szynal.com.pl>
2020-06-21 20:24:04 +0000

    Method: createWallet and endpoint in api route

--- app/Http/Controllers/API/TronController.php
+++ app/Http/Controllers/API/TronController.php
@@ -12,7 +12,24 @@ class TronController extends Controller
 
     public function createWallet()
     {
+        $fullNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
+        $solidityNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
+        $eventServer = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
 
+        try {
+          $tron = new \IEXBase\TronAPI\Tron($fullNode, $solidityNode, $eventServer);
+        } catch (\IEXBase\TronAPI\Exception\TronException $e) {
+          exit($e->getMessage());
+        }
+
+        $tron = $tron->createAccount();
+
+        $wallet = new Wallet();
+        $wallet->address = $tron['address'];
+        $wallet->secret = $tron['privateKey'];
+        $wallet->save();
+
+        return response()->json($wallet, 201);
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
+
+Route::post('/create-wallet', 'API\TronController@createWallet')->middleware('auth:api');
